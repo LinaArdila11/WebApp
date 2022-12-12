31163119f326f2a523c56f5aebd6e96670b60fd6
Lina Ardila <lmardilag@correo.udistrital.edu.co>
2022-12-12 17:29:20 -0500

    V.001

---
+++ Programa.py
@@ -0,0 +1,31 @@
+# Importar bibliotecas 
+
+from flask import *
+from flask_sqlalchemy import *
+
+
+# Crear un objeto de la clase flask
+
+app=Flask(__name__)
+# configurar el acceso a la DB
+#URL de la base de datos 
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///estudiantes.sqlite3"
+
+#Crear la DB
+
+db=SQLAlchemy(app)
+
+#Definir las tablas de la DB
+class Estudiante(db.Model):
+
+    #Definir una llave primaria: campo cuyo valor nunca se repite
+    id=db.Column('id_estudiante', db.Integer, primary_key=True)
+
+    #Definir columnas de la tabla
+
+    nombre=db.Column(db.String(50))
+    codigo=db.Column(db.String(12))
+
+    def __init__(self, nombre, codigo):
+        self.nombre=nombre
+        self.codigo=codigo
